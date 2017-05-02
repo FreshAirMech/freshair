9229f3fcf311db0f2e52e041335d18768ec1216c
Jonathan Rim <jonrim@umich.edu>
2017-05-02 16:44:50 -0400

    update phone ui, edge case for checking if email fields are empty

--- client/src/components/Settings/component.js
+++ client/src/components/Settings/component.js
@@ -8,10 +8,12 @@ export default class Settings extends Component {
     this.state = {
       newEmail: '',
       reenterNewEmail: '',
-      reenterDirty: false
+      reenterDirty: false,
+      newPhone: this.props.phone || null
     };
     this.handleChange = this.handleChange.bind(this);
-    this.submitUpdateForm = this.submitUpdateForm.bind(this);
+    this.submitPhoneForm = this.submitPhoneForm.bind(this);
+    this.submitEmailForm = this.submitEmailForm.bind(this);
   }
 
   handleChange(e) {
@@ -26,9 +28,21 @@ export default class Settings extends Component {
     });
   }
 
-  submitUpdateForm(e) {
+  submitPhoneForm(e) {
+    const { newPhone } = this.state;
+    const { requestCheckInfo, username } = this.props;
+
+    e.preventDefault();
+
+    requestCheckInfo({
+      username,
+      newPhone
+    });
+  }
+
+  submitEmailForm(e) {
     const { newEmail, reenterNewEmail } = this.state;
-    const { requestCheckInfo, username, email } = this.props;
+    const { requestCheckInfo, username } = this.props;
 
     e.preventDefault();
 
@@ -52,15 +66,21 @@ export default class Settings extends Component {
     return null;
   }
 
-  checkFormIsValid() {
-    const { newEmail, reenterNewEmail } = this.state;
+  checkFormIsValid(form) {
+    const { newEmail, reenterNewEmail, newPhone } = this.state;
     const { email } = this.props;
-    return newEmail === reenterNewEmail && newEmail !== email;
+    function isInt(value) {
+      return !isNaN(value) && (function(x) { return (x | 0) === x; })(parseFloat(value))
+    }
+    if (form === 'phone')
+      return newPhone && isInt(newPhone) && newPhone.toString().length === 10;
+    if (form === 'email')
+      return newEmail && reenterNewEmail && newEmail === reenterNewEmail && newEmail !== email;
   }
 
   render() {
     const { email, isFetching, error } = this.props;
-    const { newEmail, reenterNewEmail } = this.state;
+    const { newEmail, reenterNewEmail, newPhone } = this.state;
     return (
       <div className="settings">
         <Row id="settings-header" className="settings-row">
@@ -83,6 +103,27 @@ export default class Settings extends Component {
           <Col sm={6}>
             <Panel>
               <h3>Update Phone Settings</h3>
+              <Form onSubmit={this.submitPhoneForm}>
+                <FormGroup>
+                  <ControlLabel><label>Phone Number (10 digits, no formatting)</label></ControlLabel>
+                  <FormControl
+                    name="newPhone"
+                    type="number"
+                    value={ newPhone }
+                    onChange={this.handleChange}
+                  />
+                </FormGroup>
+
+                <FormGroup validationState="error" className="auth-form-error">
+                  <Button type="submit" disabled={ !this.checkFormIsValid('phone') } bsStyle="success">
+                    { isFetching ? <Spinner /> : 'Confirm Phone Changes' }
+                  </Button>
+                  {
+                    (!isFetching && error) && <ControlLabel className="auth-form-error-message">{ error.message }</ControlLabel>
+                  }
+                </FormGroup>
+
+              </Form>
             </Panel>
           </Col>
           <Col sm={6}>
@@ -91,7 +132,7 @@ export default class Settings extends Component {
               <label>Current Email Address</label>
               <p>{email}</p>
 
-              <Form onSubmit={this.submitUpdateForm}>
+              <Form onSubmit={this.submitEmailForm}>
                 <FormGroup>
                   <ControlLabel><label>New Email Address</label></ControlLabel>
                   <FormControl
@@ -113,7 +154,7 @@ export default class Settings extends Component {
                 </FormGroup>
 
                 <FormGroup validationState="error" className="auth-form-error">
-                  <Button type="submit" disabled={ !this.checkFormIsValid() } bsStyle="success">
+                  <Button type="submit" disabled={ !this.checkFormIsValid('email') } bsStyle="success">
                     { isFetching ? <Spinner /> : 'Confirm Email Changes' }
                   </Button>
                   {
