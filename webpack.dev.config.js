a4f585bf8a3729e6c58425534391d4dd3d68f0b3
Jonathan Rim <jonrim@umich.edu>
2017-05-26 05:51:21 -0400

    font changes

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -47,7 +47,7 @@ module.exports = {
         ]
       },
       {
-        test: /\.(eot|svg|ttf|woff|woff2)$/,
+        test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
         loader: 'file?name=fonts/[name].[ext]'
       }
     ]
