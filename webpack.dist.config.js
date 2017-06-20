423cc68ccda86abe52a19c01a17e6d44ce56fd61
Jonathan Rim <jonrim@umich.edu>
2017-06-20 04:06:28 -0400

    retry

--- webpack.dist.config.js
+++ webpack.dist.config.js
@@ -1,18 +1,68 @@
+var path = require('path');
+
+var query = {
+  bypassOnDebug: true,
+  optipng: {
+    optimizationLevel: true
+  },
+  gifsicle: {
+    interlaced: false
+  }
+};
+
 module.exports = {
-  context: __dirname + '/src',
-  entry: 'index.js',
+  entry: './client/src/index.js',
   output: {
     filename: 'bundle.js',
-    path: __dirname + '/dist/assets/'
+    path: __dirname + '/public'
   },
   resolve: {
-    extensions: ['', '.js', '.jsx', '.json']
-  }
+    extensions: ['', '.js', '.jsx', '.json', '.scss'],
+    alias: {
+      lib: path.resolve(__dirname, 'client/src/lib'),
+      actions: path.resolve(__dirname, 'client/src/actions'),
+      constants: path.resolve(__dirname, 'client/src/constants'),
+      styles: path.resolve(__dirname, 'client/src/lib/styles')
+    },
+    root: [
+      path.resolve(__dirname)
+    ]
+  },
   devtool: 'source-map',
   module: {
-    loaderszz: {
-      test: /\.jsx?$/,
-      loader: 'babel-loader'
-    }
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.json$/,
+        loader: 'json-loader'
+      },
+      {
+        test: /\.css$/,
+        loader: 'style!css'
+      },
+      {
+        test: /\.scss$/,
+        loaders: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        loaders: [
+            'file-loader?hash=sha512&digest=hex&name=[hash].[ext]',
+            'image-webpack-loader?${JSON.stringify(query)}',
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
+        loader: 'file?name=fonts/[name].[ext]'
+      }
+    ]
   }
 }
